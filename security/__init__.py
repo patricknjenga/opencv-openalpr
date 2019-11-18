b4e72956a722018b6dcd33d025db11d1f8de78bf
patricknjenga <patrick.njenga@strathmore.edu>
2019-11-18 13:32:40 +0300

    requirements

--- security/__init__.py
+++ security/__init__.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
@@ -15,10 +17,3 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
 from security.routes import routes
-
-# pip install flask_SQLAlchemy
-# pip install flask
-# pip install flask_bcrypt
-# pip install flask_login
-# pip install flask_wtf
-# pip install Pillow
